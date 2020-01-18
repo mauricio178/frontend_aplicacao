7d65f79e42b7d611e63b84bdaa2eb4395023d223
mauricio178 <mauricio.log98@hotmail.com>
2020-01-18 16:01:50 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Toda vez que for usar o HTML dentro do JavaScript Devemos importar o "React"
+
+import React, { useState }from 'react';
+
+// Componentes: É uma função que retonar um conteúdo HTML, ou seja, um Bloco isolado de HTML, CSS e JS
+// o qual não intefere no restante da aplicação.
+
+// Estado: Informações mantidas pelo componente. Obs: conceito de IMUTABILIDADE.
+ 
+// Propriedade:  mais conhecido como Atributo, são os atributos ou propriedades dos Componentes.
+//ou informações que um componente PAI passa para um componente filho.
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [contador, setContador] = useState (0);
+    function incrementar(){
+      setContador(contador+1);
+    }
+      return (
+      <>
+        <h1>Contadores : {contador} </h1>
+        <button onClick={incrementar}>Adicionar mais 1</button>
+      </>
+      );
+    
 }
 
 export default App;
